c1d78270d198f7229e6d63255c052eed9191ffe8
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2024-04-04 00:30:33 +0700

    add patient create api

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Patient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -25,5 +26,42 @@ class PatientController extends Controller
             'message' => 'Success',
             'status' => 'OK'
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nik' => 'required|string',
+            'kk' => 'required|string',
+            'name' => 'required|string',
+            'phone' => 'required|string',
+            'gender' => 'required|string',
+            'birth_place' => 'required|string',
+            'birth_date' => 'required|string',
+            'is_deceased' => 'int',
+            'address_line' => 'required|string',
+            'city' => 'string',
+            'city_code' => 'string',
+            'province' => 'string',
+            'province_code' => 'string',
+            'district' => 'string',
+            'district_code' => 'string',
+            'village' => 'string',
+            'village_code' => 'string',
+            'rt' => 'string',
+            'rw' => 'string',
+            'postal_code' => 'string',
+            'marital_status' => 'string',
+            'relationship_name' => 'nullable|string',
+            'relationship_phone' => 'nullable|string',
+        ]);
+
+        $patient = Patient::create($request->all());
+
+        return response([
+            'data' => $patient,
+            'message' => 'Patient created successfully',
+            'status' => 'Created'
+        ], 201);
     }
 }
